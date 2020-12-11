a934b5c76e44c6e877051f88f422beaa2c1c534a
Michael Alexander <michael.m.alexander@outlook.com>
2020-12-10 18:16:30 -0800

    changes to js, fixed issues with css

--- planner.js
+++ planner.js
@@ -1,3 +1,5 @@
+$(document).ready(function () {
+
 var dayPlanner = [
     {
         id: "0",
@@ -65,37 +67,163 @@ var dayPlanner = [
 
 ]
 
-// get current date and append to planner
+
 var currentDate = moment().format('dddd, MMMM Do');
+var currentHour = moment().hour();
+
+function updateTime () {
+    var currentDate = moment().format('dddd, MMMM Do');
+    var currentHour = moment().hour();
+
+    createPlanner()
+}
+
 $("header").append(currentDate);
 
-$(document).ready(function () {
+function createPlanner() {
 
     for (let i = 0; i < dayPlanner.length; i++) {
 
         var hour = dayPlanner[i].hour;
+        var hourMil = dayPlanner[i].time;
         var ampm = dayPlanner[i].ampm;
+        
 
         var makeRow = $("<form>").attr({ "class": "row" });
         $(".container").append(makeRow);
 
-        var inputField = $("<div>");
-        inputField.text(hour + ":00" + ampm);
-        inputField.attr({ "class": "col-md-2 hour" });
+        var timeField = $("<div>");
+        timeField.text(hour + ":00" + ampm);
+        timeField.attr({ "class": "col-md-2 hour" });
 
         var planBody = $("<div>");
         planBody.attr({ "class": "col-md-9" });
 
         var planInput = $("<textarea>");
-        planInput.css("background-color", "lightgray");
-        planBody.append(planInput);
-        // planInput.attr("id", dayPlanner.id);
-    
-        var buttonIcon = $("<i class='far fa-save fa-lg'></i>")
-        var saveButton = $("<button>");
-        saveButton.attr({ "class": "col-md-1 saveBtn" });
-        saveButton.append(buttonIcon);
-        makeRow.append(inputField, planBody, saveButton);
+        // planInput.css("background-color", "lightgray");
+        planInput.attr({"class": "text"});
 
-    }
+    
+        
+        if (currentHour < hourMil) {
+            planInput.attr({ "class": "future"}) 
+
+        } else if (currentHour == hourMil) {
+                planInput.attr({"class": "present"})
+            }
+            else if (currentHour > hourMil) {
+                planInput.attr({"class": "past"})
+            }
+
+            planBody.append(planInput);
+
+            var buttonIcon = $("<i class='far fa-save fa-lg'></i>")
+            var saveButton = $("<button>");
+            saveButton.attr({ "class": "col-md-1 saveBtn" });
+            saveButton.append(buttonIcon);
+            makeRow.append(timeField, planBody, saveButton);
+        }
+
+}
+createPlanner ()
+
+const id = setInterval (updateTime, 10000);
+
 })
+
+
+
+// function saveNotes() {
+//     localStorage.setItem("dayPlan", JSON.stringify(dayPlanner));
+// }
+
+// //Ability to view data in LocalStorage 
+
+// function showNotes() {
+//     dayPlanner.forEach(function (_thisHour) {
+//         $(`#${_thisHour.id}`).val(_thisHour.reminder);
+//     })
+// }
+
+
+// //Displays note data in the planner
+
+// function viewNotes() {
+//     var storedDay = JSON.parse(localStorage.getItem("dayPlan"));
+
+//     if (storedDay) {
+//         dayPlanner = storedDay;
+//     }
+
+//     saveNotes();
+//     showNotes();
+// }
+
+// // loads header date
+
+// // function renderRows () 
+// //get current time
+// //get stored notes
+// //render rows with colors and notes
+
+// dayPlanner.forEach(function(thisHour) {
+//     // creates timeblocks row
+//     var makeRow = $("<form>").attr({
+//         "class": "row"
+//     });
+//     $(".container").append(makeRow);
+
+//     // creates time field
+//     var inputField = $("<div>")
+//         .text(`${thisHour.hour}${thisHour.ampm}`)
+//         .attr({
+//             "class": "col-md-2 hour"
+//     });
+
+//     // creates scheduler data
+//     var planBody = $("<div>")
+//         .attr({
+//             "class": "col-md-9 description p-0"
+//         });
+
+//     var planInput = $("<textarea>");
+//     planBody.append(planInput);
+//     planInput.attr("id", thisHour.id);
+
+//     if (thisHour.time < moment().format("HH")) {
+//        planInput.attr ({
+//             "class": "past", 
+//         })
+//     } else if (thisHour.time === moment().format("HH")) {
+//         planInput.attr({
+//             "class": "present"
+//         })
+//     } else if (thisHour.time > moment().format("HH")) {
+//         planInput.attr({
+//             "class": "future"
+//         })
+//     }
+
+//     // creates save button
+//     var saveButton = $("<i class='far fa-save fa-lg'></i>")
+//     var savePlan = $("<button>")
+//         .attr({
+//             "class": "col-md-1 saveBtn"
+//     });
+//     savePlan.append(saveButton);
+//     makeRow.append(inputField, planBody, savePlan);
+// })
+
+// // loads any existing localstorage data after components created
+// viewNotes();
+
+
+// // saves data to be used in localStorage
+// $(".saveBtn").on("click", function(event) {
+//     event.preventDefault();
+//     var saveIndex = $(this).siblings(".description").children(".future").attr("id");
+//     dayPlanner[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
+//     console.log(saveIndex);
+//     saveNotes();
+//     showNotes();
+// })
